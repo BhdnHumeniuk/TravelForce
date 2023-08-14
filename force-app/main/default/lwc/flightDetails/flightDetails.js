ff3bdb291af2f9fb370d64be180768a52cab1456
BhdnHumeniuk <94472436+BhdnHumeniuk@users.noreply.github.com>
2023-08-14 14:04:45 +0300

    TripRepository need to update, but 76% code coverage

--- force-app/main/default/lwc/flightDetails/flightDetails.js
+++ force-app/main/default/lwc/flightDetails/flightDetails.js
@@ -23,7 +23,6 @@ export default class FlightDetails extends LightningElement {
 
     @wire(getFlightDetails, { tripId: '$recordId' })
     wiredFlightDetails(result) {
-        console.log('RESULT: ' + JSON.stringify(result));
         this.wiredFlightDetailsResult = result;
         const { data, error } = result;
         if (data && Object.keys(data).length !== 0) {
